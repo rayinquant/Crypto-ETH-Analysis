87183f9cfcd55d0e874a1ea5f62d8c195d05df1e
Raymond <134855356+wolfofquantst@users.noreply.github.com>
2024-08-01 15:16:03 +0800

    Update backtesting_script.py

--- backtesting_script.py
+++ backtesting_script.py
@@ -59,24 +59,7 @@ print(price_data)
 # change the csv filename to the token you are doing backtesting on
 price_data.to_csv('bnb_price_data.csv')
 
-# Function to place an order
-# def place_order(symbol, type, side, amount, price=None):
-#     try:
-#         order = exchange.create_order(symbol, type, side, amount, price)
-#         send_telegram_message(f"Order placed: {order}")
-#         return order
-#     except Exception as e:
 #         send_telegram_message(f"An error occurred while placing order: {e}")
 
 # place_order('BTC/USDT', 'limit', 'buy', 0.001, 30000)
 
-
-from telegram import Bot
-# Telegram bot setup
-telegram_token = '6404732928:AAFMM3jg8A7KdH0rOizCXcDNDCWKlmAcWS8'
-chat_id = '-4158563907'
-bot = Bot(token=telegram_token)
-
-# Function to send Telegram messages
-def send_telegram_message(message):
-    bot.send_message(chat_id=chat_id, text=message)
